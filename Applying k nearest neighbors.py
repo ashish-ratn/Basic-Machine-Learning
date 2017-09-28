f73aace07d9878394487bec2e63fdd1052ece721
Ashish Ratnawat <ashishratnawat452@gmail.com>
2017-09-28 16:59:43 +0530

    Add files via upload

---
+++ Applying k nearest neighbors.py
@@ -0,0 +1,29 @@
+import numpy as np
+from math import sqrt
+import warnings
+from collections import Counter
+import pandas as pd
+import random
+
+def k_nearest_neighbors(data,predict,k=3):
+     if len(data) >=k:
+        warnings.warn('K is set to a value less than total voting groups')
+    distances = []
+    for group in data:
+        for features in data[group]:
+            euclidean_distance = np.linalg.norm(np.array(features) - np.array(predict))
+            distances.append([euclidean_distance,group])
+    votes = [i[1] for i in sorted(distances)[:k]]
+    vote_result = Counter(votes).most_common(1)[0][0]
+    print(Counter(votes).most_common(1))
+    return vote_result
+
+result = k_nearest_neighbors(dataset,new_features,k=3)
+print(result)
+
+[ [plt.scatter(ii[0],ii[1],s=100,color = i) for ii in dataset[i] ] for i in dataset ]
+plt.scatter(new_features[0],new_features[1],color = result)
+plt.show()
+
+                          
+       
