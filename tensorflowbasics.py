65b922e0096d9007c6dcb5b3ea8d3e6083c8621d
Ashish Ratnawat <ashishratnawat452@gmail.com>
2017-10-04 22:57:15 +0530

    Add files via upload

---
+++ tensorflowbasics.py
@@ -0,0 +1,19 @@
+import tensorflow as tf 
+
+x1 = tf.constant([[4]])
+x2 = tf.constant([[9]])
+
+#result = x1*x2
+result = tf.matmul(x1,x2)
+
+'''print(result)
+sess = tf.Session()
+print(sess.run(result))
+sess.close()'''
+
+with tf.Session() as sess:
+	output = sess.run(result)
+	print(output)
+
+print(output)
+
